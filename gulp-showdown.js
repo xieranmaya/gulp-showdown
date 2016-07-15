69f5c06603fad6e132e5b15a28c95cb5d219aa71
Jake Dsouza <jaked@vmware.com>
2016-07-14 19:21:20 -0700

    Fix API change for converter

--- gulp-showdown.js
+++ gulp-showdown.js
@@ -1,27 +1,27 @@
-var gutil = require('gulp-util')
-var through = require('through2')
-var Showdown = require('showdown')
+var gutil = require('gulp-util');
+var through = require('through2');
+var Showdown = require('showdown');
 
 function gulpShowdown(options) {
     var defaultOptions = {
         extensions: ['table']
-    }
+    };
 
-    var converter = new Showdown.converter(options || defaultOptions)
+    var converter = new Showdown.Converter(options || defaultOptions);
 
     return through.obj(function (file, encoding, cb) {
-        var fileText = file.contents.toString()
+        var fileText = file.contents.toString();
 
-        var fileHtml = converter.makeHtml(fileText)
+        var fileHtml = converter.makeHtml(fileText);
 
-        file.contents = new Buffer(fileHtml)
+        file.contents = new Buffer(fileHtml);
 
-        file.path = gutil.replaceExtension(file.path, '.html')
+        file.path = gutil.replaceExtension(file.path, '.html');
 
-        this.push(file)
+        this.push(file);
 
-        cb(null, file)
-    })
+        cb(null, file);
+    });
 }
 
-module.exports = gulpShowdown
+module.exports = gulpShowdown;
